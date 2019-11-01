72ddb86185a80d6ad8d1c26dfb0649149a634c55
danke <danke@2dfire.com>
2019-11-01 22:54:59 +0800

    [MOD] 添加注释

--- Framework/app/src/test/java/com/study/arithmetic/Day5_sore/ExampleUnitTest.java
+++ Framework/app/src/test/java/com/study/arithmetic/Day5_sore/ExampleUnitTest.java
@@ -17,12 +17,12 @@ public class ExampleUnitTest {
 
     @Test
     public void testSort() {
-//        int[] list = new int[]{4, 7, 8, 10, 14, 21, 22, 36, 62, 77, 81, 91};
-//        printList(list);
-//        System.out.println("index = " + binarySearch(list, 0, list.length, 5));
+        int[] list = new int[]{4, 7, 8, 10, 14, 21, 22, 36, 62, 77, 81, 91};
+        printList(list);
+        System.out.println("index = " + binarySearch(list, 0, list.length, 9));
 
 
-        int[] list = new int[]{31, 68, 45, 90, 23, 39, 54, 68, 87, 76};
+//        int[] list = new int[]{31, 68, 45, 90, 23, 39, 54, 68, 87, 76};
         printList(list);
         quickSort(list, 0, list.length - 1);
         printList(list);
@@ -30,8 +30,8 @@ public class ExampleUnitTest {
 
     public int binarySearch(int[] array, int fromIndex, int toIndex, int key) {
         int low = fromIndex;
-        int high = toIndex - 1;
-        while (low <= high) {
+        int high = toIndex - 1; // 引用了左右开的原则概念
+        while (low <= high) { // 不要使用递归，循环的效率比递归高
             int mid = (low + high) >> 1; // 取中间 x >> 1 = x / 2
             int midVal = array[mid];
             if (midVal > key) {
@@ -45,16 +45,19 @@ public class ExampleUnitTest {
                 return mid;
             }
         }
-        return -(low + 1); // low+1表示找不到时停在了第low+1个元素的位置
+        return -(low + 1); // low+1表示找不到时停在了第low+1个元素的位置，+1保证返回的是负数
     }
 
     /**
-     * 快速排序
+     * 快速排序: Arrays.sort(list); 缺点：只能排顺序表，不能排链表
      * @param array
      * @param begin
      * @param end
      */
     public void quickSort(int[] array, int begin, int end) {
+        /**
+         * 1、跟
+         */
         if (begin >= end) {
             return;
         }
@@ -67,7 +70,7 @@ public class ExampleUnitTest {
         while (low < high) {
             if (direction) { // 从右往左查找
                 for (int i = high; i > low; i--) {
-                    if (array[i] <= x) {
+                    if (array[i] <= x) { // 缺点：1、如果有大量的相同数据，也会进行取值比较操作，性能就会下降
                         array[low++] = array[i];
                         high = i;
                         direction = !direction; // 更换方向
@@ -88,7 +91,13 @@ public class ExampleUnitTest {
             }
         }
         array[low] = x;
+        /**
+         * 2、左
+         */
         quickSort(array, begin, low - 1);
+        /**
+         * 3、右
+         */
         quickSort(array, low + 1, end);
     }
 
